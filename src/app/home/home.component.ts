84962740087858e4e0d3b2b79d40e8a3a9dda8ae
yatoker <yusufandre@gmail.com>
2018-09-16 13:06:42 +0300

    Building & Using a Custom Observable from Scratch

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
 import 'rxjs/Rx';
 
 @Component({
@@ -13,12 +13,38 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     //A new observable object creation using built in function interval which emits incremental number at specified interval(1sec)
-    const myNumbers = Observable.interval(1000);
+    //const myNumbers = Observable.interval(1000);
 
     //and observing the observable object (myNumbers) by subscribing to it and receiving emitted data.
-    myNumbers.subscribe((number: number) => {
-      console.log(number);
-    })
+    // myNumbers.subscribe((number: number) => {
+    //   console.log(number);
+    // })
+
+    const myObservabe = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next('first package');
+      }, 2000);
+
+      setTimeout(() => {
+        observer.next('second package');
+      }, 4000);
+
+      setTimeout(() => {
+        // observer.error("this doesn't work");
+        observer.complete();
+      }, 5000);
+
+      //This won't work as observable completed at 5th second.
+      setTimeout(() => {
+        observer.next('third package');
+      }, 6000);
+
+    });
+
+    myObservabe.subscribe(
+      (data: string) => { console.log(data); },
+      (error: string) => { console.log(error) }
+    );
 
 
   }
