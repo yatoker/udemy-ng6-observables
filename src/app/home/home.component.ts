d761073acf92cce4df6f88b19b06971d895aa924
yatoker <yusufandre@gmail.com>
2018-09-16 12:15:20 +0300

    Very simple custom observable.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +12,15 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    //A new observable object creation using built in function interval which emits incremental number at specified interval(1sec)
+    const myNumbers = Observable.interval(1000);
+
+    //and observing the observable object (myNumbers) by subscribing to it and receiving emitted data.
+    myNumbers.subscribe((number: number) => {
+      console.log(number);
+    })
+
+
   }
 
 }
